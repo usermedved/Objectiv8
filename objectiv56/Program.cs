69abc4a6f90d499a74f9835700a127946f10b6d0
Sergei Medvedev <clip1988@yandex.ru>
2022-09-04 23:13:32 +0300

    Objectiv 56 project

---
+++ objectiv56/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine();
+Console.Write("Введите размер ряда: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер столбца: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Двумерный массив: ");
+int[,] array = new int[rows, colums];
+CreateArray(array);
+WriteArray(array);
+
+int minSumRows = 0;
+int sumRows = SumRowsElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+  int temporalSumRows = SumRowsElements(array, i);
+  if (sumRows > temporalSumRows)
+  {
+    sumRows = temporalSumRows;
+    minSumRows = i;
+  }
+}
+Console.WriteLine();
+Console.WriteLine($"{minSumRows+1} - строкa с наименьшей суммой элементов ({sumRows})");
+
+
+int SumRowsElements(int[,] array, int i)
+{
+  int sumRows = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumRows += array[i,j];
+  }
+  return sumRows;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(100);
+    }
+  }
+}
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
