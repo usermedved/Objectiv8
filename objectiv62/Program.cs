f88d097cd7f8d2151f30fc42b6c5b19e770992c7
Sergei Medvedev <clip1988@yandex.ru>
2022-09-05 18:52:26 +0300

    Objectiv 62 project

---
+++ objectiv62/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine();
+Console.WriteLine("Спирально заполненный массив 4 х 4");
+Console.WriteLine();
+int RowsColums = 4;
+int[,] sqareMatrix = new int[RowsColums, RowsColums];
+
+int temporal = 1;
+int i = 0;
+int j = 0;
+
+while (temporal <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+  sqareMatrix[i, j] = temporal;
+  temporal++;
+  if (i <= j + 1 && i + j < sqareMatrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= sqareMatrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > sqareMatrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(sqareMatrix);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
